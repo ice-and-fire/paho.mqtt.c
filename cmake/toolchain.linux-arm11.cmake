cb30eb7fd8ab8880e465fc0e010d22cda39d4a93
muxuelong <xuelong.mu@nufront.com>
2021-01-01 22:05:21 +0800

    switch compile-tools to zynq 7100 platform
    Signed-off-by: muxuelong <xuelong.mu@nufront.com>

--- cmake/toolchain.linux-arm11.cmake
+++ cmake/toolchain.linux-arm11.cmake
@@ -1,6 +1,7 @@
 # path to compiler and utilities
 # specify the cross compiler
-SET(CMAKE_C_COMPILER arm-xilinx-linux-gnueabi-gcc)
+#SET(CMAKE_C_COMPILER arm-xilinx-linux-gnueabi-gcc)
+SET(CMAKE_C_COMPILER arm-linux-gnueabihf-gcc)
 
 # Name of the target platform
 SET(CMAKE_SYSTEM_NAME Linux)
